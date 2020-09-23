ef019d96eebd24226ea01757a944e0074b71b68a
Luiz Neto <luizneto@rits.com.br>
2020-09-23 17:50:54 -0300

    composer

--- public/js/cookies-policy-public.js
+++ public/js/cookies-policy-public.js
@@ -3,7 +3,7 @@
 	$(document).on("click", "#inversa-allow-cookies", function(e) {
 		e.preventDefault();
 		document.getElementById("inversa-cookies-policy-message").style.display = "none";
-		Cookies.set("permission_use_cookies", "allow");
+		Cookies.set("permission_use_cookies", "allow",{expires: 0.00001});
 		window.location.reload();
 	});
 })( jQuery );
