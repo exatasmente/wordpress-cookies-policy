ed88fa094f2b69ef0a1750235dd8c749d6f022e0
Luiz Neto <luizneto@rits.com.br>
2020-10-02 11:27:16 -0300

    update cookies policy to handle cookies via js

--- includes/class-cookies-policy.php
+++ includes/class-cookies-policy.php
@@ -16,75 +16,72 @@
  */
 class Cookies_Policy {
 
-	protected $loader;
-	protected $plugin_name;
-	protected $version;
-	public function __construct() {
-		if ( defined( 'COOKIES_POLICY_VERSION' ) ) {
-			$this->version = COOKIES_POLICY_VERSION;
-		} else {
-			$this->version = '1.0.0';
-		}
-		$this->plugin_name = 'cookies-policy';
+    protected $loader;
+    protected $plugin_name;
+    protected $version;
+    public function __construct() {
+        if ( defined( 'COOKIES_POLICY_VERSION' ) ) {
+            $this->version = COOKIES_POLICY_VERSION;
+        } else {
+            $this->version = '1.0.0';
+        }
+        $this->plugin_name = 'cookies-policy';
 
-		$this->load_dependencies();
-		$this->set_locale();
-		$this->define_admin_hooks();
-		$this->define_public_hooks();
-	}
-
-
-	private function load_dependencies() {
-
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-cookies-policy-loader.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-cookies-policy-i18n.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-cookies-policy-admin.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-cookies-policy-public.php';
-
-		$this->loader = new Cookies_Policy_Loader();
-
-	}
+        $this->load_dependencies();
+        $this->set_locale();
+        $this->define_admin_hooks();
+        $this->define_public_hooks();
+    }
 
 
-	private function set_locale() {
+    private function load_dependencies() {
 
-		$plugin_i18n = new Cookies_Policy_i18n();
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-cookies-policy-loader.php';
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-cookies-policy-i18n.php';
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-cookies-policy-admin.php';
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-cookies-policy-public.php';
 
-		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+        $this->loader = new Cookies_Policy_Loader();
 
-	}
+    }
 
-	private function define_admin_hooks() {
 
-		$plugin_admin = new Cookies_Policy_Admin( $this->get_plugin_name(), $this->get_version() );
+    private function set_locale() {
+
+        $plugin_i18n = new Cookies_Policy_i18n();
+
+        $this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+
+    }
+
+    private function define_admin_hooks() {
+
+        $plugin_admin = new Cookies_Policy_Admin( $this->get_plugin_name(), $this->get_version() );
         $this->loader->add_action( 'admin_menu', $plugin_admin, 'cookies_add_plugin_page' );
         $this->loader->add_action( 'admin_init', $plugin_admin, 'cookies_page_init' );
-	}
+    }
 
-	private function define_public_hooks() {
-		$plugin_public = new Cookies_Policy_Public( $this->get_plugin_name(), $this->get_version() );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-        $this->loader->add_action('init',$plugin_public,'allow_cookies');
-	}
-
-	public function run() {
-		$this->loader->run();
-	}
+    private function define_public_hooks() {
+        $plugin_public = new Cookies_Policy_Public( $this->get_plugin_name(), $this->get_version());
+        $this->loader->add_action('init', $plugin_public, 'cookies_policy_init');
+    }
+    public function run() {
+        $this->loader->run();
+    }
 
 
-	public function get_plugin_name() {
-		return $this->plugin_name;
-	}
+    public function get_plugin_name() {
+        return $this->plugin_name;
+    }
 
 
-	public function get_loader() {
-		return $this->loader;
-	}
+    public function get_loader() {
+        return $this->loader;
+    }
 
 
-	public function get_version() {
-		return $this->version;
-	}
+    public function get_version() {
+        return $this->version;
+    }
 
 }
--- public/js/cookies-policy-public.js
+++ public/js/cookies-policy-public.js
@@ -1,9 +1,55 @@
-(function( $ ) {
+var cookies_message;
+window.Cookies_Policy = (function( $ ) {
 	'use strict';
-	$(document).on("click", "#allow-cookies-button", function(e) {
-		e.preventDefault();
-		document.getElementById("allow-cookies-policy-component").style.display = "none";
-		Cookies.set("allow_cookies", "allow",{expires: 0.0001});
-		window.location.reload();
-	});
+	function setCookies(name,value,expirationInDays= 1,path = "/",domain = null,samesite=null,secure=false) {
+		const date = new Date();
+		date.setTime(date.getTime() + (expirationInDays * 24 * 60 * 60 * 1000));
+		document.cookie = name + '=' + value
+			+ ';expires=' + date.toUTCString()
+			+ ';path='+path
+			+ (domain ? ';domain='+domain : '')
+			+ (secure ? ';secure' : '')
+			+ ';samesite='+ ( samesite ?? 'Lax');
+	}
+	function cookieExists(name,value) {
+		return document.cookie.split(';').filter(
+			(cookie) => cookie.trim() == name+'='+value
+		).length > 0;
+
+	}
+	function getDomain(){
+		if(document.domain.length){
+			let parts = document.domain.replace(/^(www\.)/,"").split('.');
+			while(parts.length > 2){
+				parts.shift();
+			}
+			let domain = parts.join('.');
+
+			return domain.replace(/(^\.*)|(\.*$)/g, "");
+		}
+		return '';
+	}
+	if (!cookieExists('permission_use_cookies','allow')) {
+		document.addEventListener('DOMContentLoaded', function () {
+			if(!document.getElementById("allow-cookies-policy-component")) {
+				document.body.insertAdjacentHTML('afterBegin', cookies_message.html);
+			}
+			$(document).on("click", "#allow-cookies-button", function (e) {
+				e.preventDefault();
+				$("#allow-cookies-policy-component")
+					.fadeOut()
+					.fadeOut("slow")
+					.fadeOut(500);
+				setCookies('permission_use_cookies','allow',1085,'/',getDomain());
+				window.location.reload();
+			});
+		})
+	}else if(document.getElementById("allow-cookies-policy-component")){
+		document.getElementById("allow-cookies-policy-component").remove();
+	}
+	return {
+		getDomain : getDomain,
+		cookieExists : cookieExists,
+	}
+
 })( jQuery );
